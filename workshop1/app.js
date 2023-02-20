d848e78ae819f4963e166c001aa083732e286436
Shangs <shangru@dsaid.gov.sg>
2023-02-20 15:29:05 +0800

    v1

---
+++ workshop1/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+const port = 8080;
+
+let quotes = [
+  "Logic will get you from A to B. Imagination will take you everywhere.",
+  "There are 10 kinds of people. Those who know binary and those who don't.",
+  "There are two ways of constructing a software design. One way is to make it so simple that there are obviously no deficiencies and the other is to make it so complicated that there are no obvious deficiencies.",
+  "It's not that I'm so smart, it's just that I stay with problems longer.",
+  "It is pitch dark. You are likely to be eaten by a grue.",
+];
+
+app.set("view engine", "ejs");
+app.get("/", function (req, res) {
+  let quoteCounter = Math.floor(Math.random() * (quotes.length - 1)) + 1;
+  res.render("pages/index", { quote: quotes[quoteCounter] });
+});
+app.use(express.static("public"));
+app.use("/images", express.static("images"));
+
+// Server setup
+app.listen(port, () => {
+  console.log(`Running server on PORT ${port}...`);
+});
